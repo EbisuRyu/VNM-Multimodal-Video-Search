fcd17e5cbf4391511c8a9c811bb59c543dc313c0
LONG <doituyentoanlequydon@gmail.com>
2024-09-17 00:38:01 +0700

    Optimize model initialization process - Updated embedding-based search to only require passing the engine. - Improved model initialization in embedding space to occur only once.

--- utils/embedding_based_search/embedding_based_search.py
+++ utils/embedding_based_search/embedding_based_search.py
@@ -1,39 +1,13 @@
-from utils.embedding_based_search.clip_engine import CLIP
-from utils.embedding_based_search.blip_engine import BLIP
-from utils.embedding_based_search.beit_engine import BEIT
 from utils.combine_module.utils import merge_searching_results_by_addition
-
-CLIP_DIR = './dict/clip'
-BLIP_DIR = './dict/blip'
-BEIT_DIR = './dict/beit'
 
 
 class EmbeddingBasedSearch:
 
-    def __init__(self, use_clip_h14: bool, use_clip_l14: bool, use_blip: bool, use_beit: bool):
-        if use_clip_h14:
-            self.clip_h14_engine = CLIP(
-                clip_bin_file=f'{CLIP_DIR}/h14_laion2b.bin',
-                clip_id2image_path=f'{CLIP_DIR}/h14_laion2b.json',
-                clip_model='ViT-H-14'
-            )
-        if use_clip_l14:
-            self.clip_l14_engine = CLIP(
-                clip_bin_file=f'{CLIP_DIR}/l14_laion400m.bin',
-                clip_id2image_path=f'{CLIP_DIR}/l14_laion400m.json',
-                clip_model='ViT-L-14'
-            )
-        if use_blip:
-            self.blip_engine = BLIP(
-                blip_bin_file=f'{BLIP_DIR}/blip_vit.bin',
-                blip_id2image_path=f'{BLIP_DIR}/blip_vit.json',
-                model_type='pretrain_vitL'
-            )
-        if use_beit:
-            self.beit_engine = BEIT(
-                beit_bin_file=f'{BEIT_DIR}/beit.bin',
-                beit_id2image_path=f'{BEIT_DIR}/beit.json'
-            )
+    def __init__(self, clip_h14_engine=None, clip_l14_engine=None, blip_engine=None, beit_engine=None):
+        self.clip_h14_engine = clip_h14_engine
+        self.clip_l14_engine = clip_l14_engine
+        self.blip_engine = blip_engine
+        self.beit_engine = beit_engine 
         self.searching_mode = {
             'clip_h14_engine': True,
             'clip_l14_engine': True,
