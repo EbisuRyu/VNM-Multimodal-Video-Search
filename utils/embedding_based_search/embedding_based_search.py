b1faabe7c57e3d036113953b6cb74912707c84a9
LONG <doituyentoanlequydon@gmail.com>
2024-09-20 22:34:50 +0700

    Add source code for extraction using CLIP-H14-xlm and fix paths - Implemented keyframe extraction using CLIP-H14-xlm model. - Fixed and updated paths for correct model and data file locations.

--- utils/embedding_based_search/embedding_based_search.py
+++ utils/embedding_based_search/embedding_based_search.py
@@ -3,24 +3,30 @@ from utils.combine_module.utils import merge_searching_results_by_addition
 
 class EmbeddingBasedSearch:
 
-    def __init__(self, clip_h14_engine=None, clip_l14_engine=None, blip_engine=None, beit_engine=None):
+    def __init__(self, clip_h14_engine=None, clip_h14_xlm_engine=None, clip_l14_engine=None, blip_engine=None, beit_base_engine=None, beit_large_engine=None):
         self.clip_h14_engine = clip_h14_engine
+        self.clip_h14_xlm_engine = clip_h14_xlm_engine
         self.clip_l14_engine = clip_l14_engine
         self.blip_engine = blip_engine
-        self.beit_engine = beit_engine 
+        self.beit_base_engine = beit_base_engine
+        self.beit_large_engine = beit_large_engine 
         self.searching_mode = {
             'clip_h14_engine': True,
+            'clip_h14_xlm_engine': True,
             'clip_l14_engine': True,
             'blip_engine': True,
-            'beit_engine': True
+            'beit_base_engine': True,
+            'beit_large_engine': True
         }
 
-    def update_searching_mode(self, clip_h14_engine=True, clip_l14_engine=True, blip_engine=True, beit_engine=True):
+    def update_searching_mode(self, clip_h14_engine=True, clip_h14_xlm_engine=True, clip_l14_engine=True, blip_engine=True, beit_base_engine=True, beit_large_engine=True):
         self.searching_mode = {
             'clip_h14_engine': clip_h14_engine,
+            'clip_h14_xlm_engine': clip_h14_xlm_engine,
             'clip_l14_engine': clip_l14_engine,
             'blip_engine': blip_engine,
-            'beit_engine': beit_engine
+            'beit_base_engine': beit_base_engine,
+            'beit_large_engine': beit_large_engine
         }
 
     def image_search(self, query_image_path, top_k):
@@ -32,7 +38,15 @@ class EmbeddingBasedSearch:
                 top_k=top_k
             )
             list_results.append(result)
-
+            
+        if self.searching_mode['clip_h14_xlm_engine']:
+            result = self.clip_h14_xlm_engine.image_search(
+                query_image_path=query_image_path,
+                image_path_subset=None,
+                top_k=top_k
+            )
+            list_results.append(result)
+            
         if self.searching_mode['clip_l14_engine']:
             result = self.clip_l14_engine.image_search(
                 query_image_path=query_image_path,
@@ -41,8 +55,16 @@ class EmbeddingBasedSearch:
             )
             list_results.append(result)
 
-        if self.searching_mode['beit_engine']:
-            result = self.beit_engine.image_search(
+        if self.searching_mode['beit_base_engine']:
+            result = self.beit_base_engine.image_search(
+                query_image_path=query_image_path,
+                image_path_subset=None,
+                top_k=top_k
+            )
+            list_results.append(result)
+        
+        if self.searching_mode['beit_large_engine']:
+            result = self.beit_large_engine.image_search(
                 query_image_path=query_image_path,
                 image_path_subset=None,
                 top_k=top_k
@@ -63,6 +85,14 @@ class EmbeddingBasedSearch:
             )
             list_results.append(result)
 
+        if self.searching_mode['clip_h14_xlm_engine']:
+            result = self.clip_h14_xlm_engine.text_search(
+                query_text=query_text,
+                image_path_subset=image_path_subset,
+                top_k=top_k
+            )
+            list_results.append(result)
+            
         if self.searching_mode['clip_l14_engine']:
             result = self.clip_l14_engine.text_search(
                 query_text=query_text,
@@ -79,8 +109,16 @@ class EmbeddingBasedSearch:
             )
             list_results.append(result)
 
-        if self.searching_mode['beit_engine']:
-            result = self.beit_engine.text_search(
+        if self.searching_mode['beit_base_engine']:
+            result = self.beit_base_engine.text_search(
+                query_text=query_text,
+                image_path_subset=image_path_subset,
+                top_k=top_k
+            )
+            list_results.append(result)
+        
+        if self.searching_mode['beit_large_engine']:
+            result = self.beit_large_engine.text_search(
                 query_text=query_text,
                 image_path_subset=image_path_subset,
                 top_k=top_k
